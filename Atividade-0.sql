3b2c7ab8df8ae0ae74b692dc4adce768bfa7ff90
GustavofuckEngels <gengels20@gmail.com>
2026-08-13 20:14:40 -0300

    testando o banco

---
+++ Atividade-0.sql
@@ -0,0 +1,13 @@
+/*--------Bloco 0 testando o banco-----------*/
+USE ecommerce_nexashop;
+SELECT
+'clientes' AS tabela, COUNT(*) AS total FROM clientes
+UNION ALL
+SELECT
+'produtos' AS tabela, COUNT(*) AS total FROM produtos
+UNION ALL
+SELECT
+'pedidos' AS tabela, COUNT(*) AS total FROM pedidos
+UNION ALL
+SELECT
+'avaliacoes' AS tabela, COUNT(*) AS total FROM avaliacoes;
